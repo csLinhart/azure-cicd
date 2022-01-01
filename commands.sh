6391238098ed5a23d1920eeb9bada49ffeee4485
Christoph Linhart <christoph.linhart@bmw.de>
2022-01-01 13:11:29 +0100

    Add commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,6 @@
-git clone git@github.com:csLinhart/udacity-azure2.git
+git clone git@github.com:csLinhart/udacity-azure-2.git
+cd udacity-azure-2
+make all
 az webapp up -n flask-ml-service-cl-udacity -l southcentralus --sku B1
 chmod +x make_predict_azure_app.sh && ./make_predict_azure_app.sh
 az webapp log tail -g lohner.csl_rg_0179 -n flask-ml-service-CL-udacity
