e39f08e60ced81878026e6f8c94c7dee1f37c4c9
Christoph Linhart <christoph.linhart@bmw.de>
2022-01-01 13:59:26 +0100

    Add locust

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, task, between
 
 class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+    wait_time = between(0.1, 5.0)
 
     @task(1)
     def test_root(self):
